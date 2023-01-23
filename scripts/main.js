a6dc35b607b939427c724b34203ef8aaa3a7d446
jjnich <jake.j.nicholson@gmail.com>
2023-01-23 14:38:41 -0600

    remove debug console output

--- scripts/main.js
+++ scripts/main.js
@@ -148,7 +148,6 @@ Hooks.on('updateItem', async (weapon, update, options, userID) => {
 
     // delete the old entry to avoid duplicate spellcasting entries
     const { actor } = weapon;
-    console.log("update - actor: " + actor);
     const spellcastingEntries = actor.items.filter(i => i.type === 'spellcastingEntry');
     const oldspellcastingEntry = spellcastingEntries.find(i => i.getFlag(moduleID, 'staveID') === weapon.id);
     if (oldspellcastingEntry) oldspellcastingEntry.delete();
